9ad1e94eaabbb049e0517a224becb89e75a39a01
Quincy <quincy.gomescedeno@mymail.champlain.edu>
2022-09-15 15:55:24 -0400

    Separation
    The somewhat okay code

--- examples/flocking/behaviours/SeparationRule.cpp
+++ examples/flocking/behaviours/SeparationRule.cpp
@@ -17,7 +17,7 @@ Vector2 SeparationRule::computeForce(const std::vector<Boid*>& neighborhood, Boi
         {
             separatingForce += neighborhood[i]->getPosition();
 
-            if( Vector2::getDistance(neighborhood[i]->getPosition(), neighborhood[i + 1]->getPosition()) < desiredDistance )\
+            if( Vector2::getDistance(neighborhood[i]->getPosition(), neighborhood[i + 1]->getPosition()) < desiredDistance )
             {
 
             }
